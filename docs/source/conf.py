20ab315bf15a01bb50260d2140f7d562fc77b658
lhock17 <levi.hockey@gmail.com>
2025-02-21 14:02:29 +1000

    Simplify sys.path modification in Sphinx configuration for improved clarity

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+sys.path.insert(0, os.path.abspath('../../src'))
 sys.path.insert(0, os.path.abspath('../../'))
 
 project = 'MMCCI'
